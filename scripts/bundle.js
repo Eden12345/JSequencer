7c9344976bbac4fd3a8b58ed4cfa080ac86b695c
Eden <ethangooding@gmail.com>
2019-07-13 23:49:43 -0400

    Change instruction flow and modal opacity

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -25020,8 +25020,8 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 const instructions = [
   "Welcome! Press Next to continue through the instructions, or press Skip to head straight to the JSequencer.",
   "The grid of buttons represents a series of sounds that will be played in a loop. Each color corresponds to a different instrument. <br> <br> blue = drums | purple = vocals | green = chords | yellow = synthesizer",
-  "When the JSequencer is playing, it will highlight which sounds are being played at that moment in the loop.",
-  "The main controls are on the left. The triangle at the top is the play button (which will turn into a square stop button after you click it). <br> <br> The slider adjusts the speed at which the JSequencer will play.",
+  "When the JSequencer is playing, it will highlight which sounds are being played at that moment in your loop.",
+  "The main controls are on the left. The triangle at the top is the play button (which will turn into a square stop button while the JSequencer is playing). <br> <br> The slider adjusts the speed at which your loop will play.",
   "The randomize button will randomly change the speed and all the buttons on the grid. The clear button will clear the entire grid.",
   "The example sequence might give you a better idea of how the JSequencer works. Try playing it after you click Finish!"
 ];
@@ -25042,14 +25042,19 @@ const step = (clickEvent) => {
       $("#back").html("");
       break;
     case 1:
-    case 3:
       $("#back").html("< Back");
+      $(".controls").css("visibility", "hidden");
+      if ($(".play-stop-button").hasClass("playing")) {
+        $(".play-stop-button").trigger("click");
+      }
+    case 3:
       if ($(".play-stop-button").hasClass("playing")) {
         $(".play-stop-button").trigger("click");
       }
       break;
     case 2:
       $(".play-stop-button").trigger("click");
+      $(".controls").css("visibility", "visible");
       break;
     case 5:
       $("#skip").css("visibility", "hidden");
@@ -25071,6 +25076,7 @@ const step = (clickEvent) => {
 };
 
 const skip = (clickEvent) => {
+  $(".controls").css("visibility", "visible");
   $(".instructions").css("display", "none");
 };
 
